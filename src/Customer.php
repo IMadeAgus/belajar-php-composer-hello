79537e2d59019a88a068031e2d06dc4730d18810
Made Agus <agsmade@gmail.com>
2024-03-26 22:16:48 +0800

    Add default value

---
+++ src/Customer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar;
+
+class Customer{
+
+    public function __construct(private string $name){
+
+    }
+    public function sayHello(string $name){
+        return "Hello $name, My name is $this->name";
+    }
+}
